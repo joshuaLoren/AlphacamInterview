67c4fed2617c3695b57ca593c142d6aca6526ea5
Joshua Small <jsmall17@uncc.edu>
2019-03-01 22:00:48 -0500

    added comments, and redesigned windows form GUI

--- AlphacamInterviewWFA/Form1.Designer.cs
+++ AlphacamInterviewWFA/Form1.Designer.cs
@@ -29,73 +29,75 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
-            this.button2 = new System.Windows.Forms.Button();
-            this.textBox5 = new System.Windows.Forms.TextBox();
-            this.tableLayoutPanel2.SuspendLayout();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // tableLayoutPanel2
+            // tableLayoutPanel1
             // 
-            this.tableLayoutPanel2.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
-            this.tableLayoutPanel2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("tableLayoutPanel2.BackgroundImage")));
-            this.tableLayoutPanel2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
-            this.tableLayoutPanel2.ColumnCount = 2;
-            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 374F));
-            this.tableLayoutPanel2.Controls.Add(this.textBox5, 0, 2);
-            this.tableLayoutPanel2.Controls.Add(this.button2, 1, 2);
-            this.tableLayoutPanel2.Location = new System.Drawing.Point(92, 41);
-            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
-            this.tableLayoutPanel2.RowCount = 4;
-            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 100F));
-            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            this.tableLayoutPanel2.Size = new System.Drawing.Size(759, 369);
-            this.tableLayoutPanel2.TabIndex = 1;
-            this.tableLayoutPanel2.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel2_Paint);
+            this.tableLayoutPanel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("tableLayoutPanel1.BackgroundImage")));
+            this.tableLayoutPanel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.textBox2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.button1, 0, 2);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1146, 543);
+            this.tableLayoutPanel1.TabIndex = 0;
+            this.tableLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel1_Paint);
             // 
-            // button2
+            // button1
             // 
-            this.button2.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.button2.Location = new System.Drawing.Point(423, 281);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(297, 33);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "Open File Explorer";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button1.Location = new System.Drawing.Point(514, 445);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(118, 59);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Choose file directory";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
             // 
-            // textBox5
+            // textBox2
             // 
-            this.textBox5.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.textBox5.Location = new System.Drawing.Point(89, 287);
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(206, 22);
-            this.textBox5.TabIndex = 6;
-            this.textBox5.Text = "Choose were to save the file";
-            this.textBox5.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.textBox2.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.textBox2.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.textBox2.Location = new System.Drawing.Point(328, 347);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(490, 56);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.Text = resources.GetString("textBox2.Text");
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(940, 504);
-            this.Controls.Add(this.tableLayoutPanel2);
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClientSize = new System.Drawing.Size(1158, 560);
+            this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "Form1";
             this.Text = "AlphacamInterview";
-            this.tableLayoutPanel2.ResumeLayout(false);
-            this.tableLayoutPanel2.PerformLayout();
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
-        private System.Windows.Forms.TextBox textBox5;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- AlphacamInterviewWFA/Form1.cs
+++ AlphacamInterviewWFA/Form1.cs
@@ -10,9 +10,9 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 
 /*
- * 
- * 
- * 
+ *    This WPF will save a text file with 1000 randomly generated integers 
+ *    to a directory of the user's choosing. The integers generated will be
+ *    between 0 and 100,000 and will not be divisible by 3 and does not end in 3.
  */
 namespace AlphacamInterviewWFA
 {
@@ -23,36 +23,37 @@ namespace AlphacamInterviewWFA
             InitializeComponent();
         }
 
-        private void tableLayoutPanel2_Paint(object sender, PaintEventArgs e)
-        {
-
-        }
-
         /*
-         * 
-         * 
+         *    Allows the user to select a directory. Once a 
+         *    directory is selected a text file will be generated and 
+         *    a dialogue box will show the user where their text file was 
+         *    saved.
          */
-        private void button2_Click(object sender, EventArgs e)
+        private void button1_Click_1(object sender, EventArgs e)
         {
             FolderBrowserDialog browse = new FolderBrowserDialog();
             browse.RootFolder = Environment.SpecialFolder.Desktop;
             browse.Description = "Choose a directory to save your text file";
             browse.ShowNewFolderButton = false;
 
-
+            //If the user selects a directory
             if (browse.ShowDialog() == System.Windows.Forms.DialogResult.OK)
             {
-
                 using (StreamWriter outputFile = new StreamWriter(Path.Combine(browse.SelectedPath, "1000numbers.txt")))
                 {
                     Random randomNumber = new Random();
                     int count = 0;
-                   
+
                     outputFile.Write("1000 random numbers" + Environment.NewLine);
                     outputFile.Write(Environment.NewLine);
-                    while (count<=1000)
+
+                    //Only increment the counter if the random number generated is not
+                    //divisble by 3 and does not end in 3.
+                    while (count <= 1000)
                     {
-                        int temp = randomNumber.Next(0, 10000);                          
+                        int temp = randomNumber.Next(0, 100000);
+                        //if temp % 3 != then the random number is not divisble by 3, or
+                        //if temp # 10 != 3 then the last digit is not the number 3
                         if (temp % 3 != 0 && temp % 10 != 3)
                         {
                             outputFile.Write("{0:N0}", temp + Environment.NewLine);
@@ -60,10 +61,26 @@ namespace AlphacamInterviewWFA
                         }
                     }
                 }
-
-                    MessageBox.Show("1000numbers.txt generated in: " + browse.SelectedPath);
-  
+                MessageBox.Show("1000numbers.txt generated in: " + browse.SelectedPath);
             }
         }
+
+  
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
     }
 }
