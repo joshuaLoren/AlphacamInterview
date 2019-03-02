bea59e527a5e0e04d0a4e51e696ae078c01e621e
Joshua Small <jsmall17@uncc.edu>
2019-03-01 20:51:44 -0500

    Printing random ints to text file if int % 3 != 0 && int % 10 != 3

--- AlphacamInterviewWFA/Form1.cs
+++ AlphacamInterviewWFA/Form1.cs
@@ -9,6 +9,11 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+/*
+ * 
+ * 
+ * 
+ */
 namespace AlphacamInterviewWFA
 {
     public partial class Form1 : Form
@@ -18,13 +23,15 @@ namespace AlphacamInterviewWFA
             InitializeComponent();
         }
 
-
         private void tableLayoutPanel2_Paint(object sender, PaintEventArgs e)
         {
 
         }
 
-
+        /*
+         * 
+         * 
+         */
         private void button2_Click(object sender, EventArgs e)
         {
             FolderBrowserDialog browse = new FolderBrowserDialog();
@@ -32,11 +39,29 @@ namespace AlphacamInterviewWFA
             browse.Description = "Choose a directory to save your text file";
             browse.ShowNewFolderButton = false;
 
+
             if (browse.ShowDialog() == System.Windows.Forms.DialogResult.OK)
             {
-                using (StreamWriter ouputFile = new StreamWriter(Path.Combine(browse.SelectedPath, "1000numbers.txt"))) ;
-     
-                MessageBox.Show("1000numbers.txt generated in: " + browse.SelectedPath);
+
+                using (StreamWriter outputFile = new StreamWriter(Path.Combine(browse.SelectedPath, "1000numbers.txt")))
+                {
+                    Random randomNumber = new Random();
+                    int count = 0;
+                   
+                    outputFile.Write("1000 random numbers" + Environment.NewLine);
+                    outputFile.Write(Environment.NewLine);
+                    while (count<=1000)
+                    {
+                        int temp = randomNumber.Next(0, 10000);                          
+                        if (temp % 3 != 0 && temp % 10 != 3)
+                        {
+                            outputFile.Write("{0:N0}", temp + Environment.NewLine);
+                            count++;
+                        }
+                    }
+                }
+
+                    MessageBox.Show("1000numbers.txt generated in: " + browse.SelectedPath);
   
             }
         }
